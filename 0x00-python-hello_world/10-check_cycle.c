61f3b3c24552a6e4fa2f9ee9a9edd6061d14dd8a
AMIRA <amira@AMIRA.myguest.virtualbox.org>
2023-11-04 16:18:25 +0200

    trial 2

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -13,12 +13,12 @@ int check_cycle(listint_t *list)
   listint_t *slow = list;
   listint_t *fast = list;
 
-  while(fast & fast->next)
+  while(fast && fast->next)
     {
       slow = slow->next;
       fast = fast->next->next;
       if (slow == fast)
-	return(1)
+	return(1);
 	  }
-  return (0)
+  return (0);
     }
