a9560255f221dad73492e2ea191c67c08807b1fa
amira-medhat <amiramedhat2016@gmail.com>
2023-11-28 00:11:57 +0200

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ Defining an empty class
 class Rectangle:
     """ class of rectangle """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
         @property
         def width(self):
